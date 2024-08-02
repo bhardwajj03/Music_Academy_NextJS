36869d17addd116cc5625471c3ef250d4973e6cf
bhardwajj03 <adi.bhardwaj09@gmail.com>
2024-08-02 12:35:06 +0530

    Home_Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import HeroSection from "./components/HeroSection";
 
 
 export default function Home(){
   return(
     <>
     <main className="min-h-screen bg-blac/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">Chai aur code</h1>
+      
+      <HeroSection />
     </main>
     </>
   )
